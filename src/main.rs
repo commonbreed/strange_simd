9c950dce17cc57dc10b4f50357b5eca2600e469e
Benjamin Mordaunt <crawford.benjamin15@gmail.com>
2023-09-27 03:18:32 +0100

    Improvements

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 use std::arch::aarch64::{int16x4_t, vmla_s16};
 use std::cmp::min;
+use std::fmt::Debug;
 use std::mem::{transmute, zeroed};
+
+// Requires allocation of a new length-4 vector on the stack consisting of
+// v[i..(i + maski)] + [0] * (4 - maski)
+fn padding_slow_path<T: Copy + Debug + Default>(in_vecs: &[T]) -> [T; 4] {
+    let mut vecs_vec = in_vecs.to_vec();
+    vecs_vec.resize(4, Default::default());
+    vecs_vec.try_into().unwrap()
+}
+
+unsafe fn masked_slice_transmute(in_vecs: &[i16]) -> int16x4_t {
+    transmute::<[i16; 4], int16x4_t>(
+        <[i16; 4]>::try_from(in_vecs).unwrap_or_else(|_| {
+            padding_slow_path(&in_vecs)
+        })
+    )
+}
 
 fn main() {
     let veca = [1i16, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -11,20 +28,8 @@ fn main() {
         let a: int16x4_t = zeroed();
         for i in (0..veca.len()).step_by(4) {
             let maski = min(veca.len() - i, 4);
-            let b: int16x4_t = transmute::<[i16; 4], int16x4_t>(
-                veca[i..(i+maski)].try_into().unwrap_or_else(|_| {
-                    // Padding slow-path
-                    let mut vecas_vec = veca[i..(i+maski)].to_vec();
-                    vecas_vec.resize(4, 0);
-                    vecas_vec.try_into().unwrap()
-                }));
-            let c: int16x4_t = transmute::<[i16; 4], int16x4_t>(
-                vecb[i..(i+maski)].try_into().unwrap_or_else(|_| {
-                    // Padding slow-path
-                    let mut vecbs_vec = vecb[i..(i+maski)].to_vec();
-                    vecbs_vec.resize(4, 0);
-                    vecbs_vec.try_into().unwrap()
-                }));
+            let b: int16x4_t = masked_slice_transmute(&veca[i..(i + maski)]);
+            let c: int16x4_t = masked_slice_transmute(&vecb[i..(i + maski)]);
             vecr[i..(i+maski)].copy_from_slice(&transmute::<int16x4_t, [i16; 4]>(vmla_s16(a, b, c))[..maski]);
         }
         for elem in vecr {
